4cd55410f16c52c62cd415c80448dffadce45328
imalhaas <lucasdecassia9@hotmail.com>
2023-01-05 21:30:36 -0300

    Metodos Delete, Put, GetOne, moficaçoes nos cruds e implemetaçao do jackson para formatar a data.

--- src/main/java/com/api/Parkingcontrol/Controller/ParkingSpotController.java
+++ src/main/java/com/api/Parkingcontrol/Controller/ParkingSpotController.java
@@ -5,6 +5,10 @@ import com.api.Parkingcontrol.Model.ParkingSpotModel;
 import com.api.Parkingcontrol.Service.ParkingSpotService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -40,4 +47,44 @@ public class ParkingSpotController {
         parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ParkingSpotModel>> getAllParkingSpots(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC)Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.findAll(pageable));
+    }
+
+        @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneParkingSpot(@PathVariable(value = "id")UUID id ){
+            Optional<ParkingSpotModel> parkingSpotModelOptional= parkingSpotService.findById(id);
+            if(!parkingSpotModelOptional.isPresent()){
+                return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parking Spot Not Found.");
+            }
+            return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Object> deleteParkingSpot(@PathVariable(value = "id")UUID id ){
+            Optional<ParkingSpotModel> parkingSpotModelOptional= parkingSpotService.findById(id);
+            if(!parkingSpotModelOptional.isPresent()){
+                return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parking Spot Not Found.");
+            }
+            parkingSpotService.delete(parkingSpotModelOptional.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Parking Spot Delete ok.");
+        }
+
+        @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParkingSpot(@PathVariable(value = "id") UUID id,
+                                                   @RequestBody @Valid ParkingSpotDtos parkingSpotDtos ){
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+        if (!parkingSpotModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parking spot Not Found");
+        }
+            var parkingSpotModel = new ParkingSpotModel();
+            BeanUtils.copyProperties(parkingSpotDtos, parkingSpotModel);
+            parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+            parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
+
+        return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
+        }
+
  }
--- src/main/java/com/api/Parkingcontrol/Service/ParkingSpotService.java
+++ src/main/java/com/api/Parkingcontrol/Service/ParkingSpotService.java
@@ -3,8 +3,14 @@ package com.api.Parkingcontrol.Service;
 import com.api.Parkingcontrol.Model.ParkingSpotModel;
 import com.api.Parkingcontrol.Repository.ParkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
@@ -28,5 +34,17 @@ public class ParkingSpotService {
 
     public boolean existsByApartmentAndBlock(String apartment, String block) {
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+    public Page<ParkingSpotModel> findAll(Pageable pageable){
+        return parkingSpotRepository.findAll(pageable);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+         parkingSpotRepository.delete(parkingSpotModel);
     }
 }
